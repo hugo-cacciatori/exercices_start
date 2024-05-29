5d90ebf152c4786ea580c16933119b98395084e3
Hugo <hugo.cacciatori.pro@gmail.com>
2024-05-29 12:29:38 +0200

    exo 06

--- 06_composition.js
+++ 06_composition.js
@@ -8,7 +8,8 @@
  *  - for, foreach, while, do while sont interdits
  */
 
-const concat = (arr1, arr2) => {}
+const concat = (arr1, arr2) => [...arr1,...arr2]
+
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
@@ -19,7 +20,7 @@ const concat = (arr1, arr2) => {}
  * - ne pas utiliser la méthode push
  */
 
-const push = (arr, item) => {}
+const push = (arr, item) => [...arr,item]
 
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
@@ -27,7 +28,7 @@ const push = (arr, item) => {}
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge = (obj1, obj2) => {}
+const merge = (obj1, obj2) => ({...obj1, ...obj2})
 
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
@@ -38,7 +39,8 @@ const merge = (obj1, obj2) => {}
  *  - interdiction d'utiliser l'opérateur d'affectation "="
  */
 
-const setName = (obj, name) => {}
+const setName = (obj, name) => ({...obj,...{name:name}})
+
 
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
 module.exports = {concat, push, merge, setName}
